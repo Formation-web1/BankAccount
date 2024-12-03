f36554c358a385b1e40e1e6bea91770cd6561e58
Elizabeth <189478429+Formation-web1@users.noreply.github.com>
2024-12-03 16:19:22 +0100

    exported class BankAccount

--- BankAccount.js
+++ BankAccount.js
@@ -1,29 +1,30 @@
-#balance = 0; // private field for balance
+export class BankAccount {
+    #balance = 0; // private field
 
 
-// Method to perform operations
-operation(transaction, amount) {
-    if (amount > 0) {
+    // Method to perform operations
+    operation(transaction, amount) {
+        if (amount > 0) {
 
-        if (transaction === "deposit") {
-            this.#balance += amount; // Deposit amount to balance
+            if (transaction === "deposit") {
+                this.#balance += amount; // Deposit amount to balance
 
-            console.log(`Deposit: ${amount}, New balance: ${this.#balance}`);
+                console.log(`Deposit: ${amount}, New balance: ${this.#balance}`);
 
-        } else if (transaction === "withdraw" && amount <= this.#balance) {
-            this.#balance -= amount; // Withdraw amount from balance
+            } else if (transaction === "withdraw" && amount <= this.#balance) {
+                this.#balance -= amount; // Withdraw amount from balance
 
-            console.log(`Withdraw: ${amount}, New balance: ${this.#balance}`);
-        } else {
-            console.log("Insufficient funds or invalid operation.");
-        }
-        } else {
-        console.log("Amount must be greater than zero.");
-        }
-}
+                console.log(`Withdraw: ${amount}, New balance: ${this.#balance}`);
+            } else {
+                console.log("Insufficient funds or invalid operation.");
+            }
+            } else {
+            console.log("Amount must be greater than zero.");
+            }
+    }
 
-// Method to display the current balance
-showBalance() {
-    console.log(`Current balance: ${this.#balance}`);
-}
+    // Method to display the current balance
+    showBalance() {
+        console.log(`Current balance: ${this.#balance}`);
+    }
 }
---
+++ main.js
@@ -0,0 +1,6 @@
+
+
+let account = new BankAccount();
+account.operation('deposit', 100);
+account.operation('withdraw', 30);
+account.showBalance();
